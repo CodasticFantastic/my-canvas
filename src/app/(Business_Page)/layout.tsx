8521cba7d37c426d3f4a218ae6e238f3db88c39f
Jakub Wojtysiak <it.jakub.wojtysiak@gmail.com>
2025-11-13 06:12:28 +0100

    Project structuring + Prettier

---
+++ src/app/(Business_Page)/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/styles/globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - My Canvas",
+    default: "My Canvas",
+  },
+  description: "My Canvas", //TODO: Add SEO description
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <body className={`${geistSans.variable} ${geistMono.variable} flex h-screen flex-col antialiased`}>
+        <header className="flex items-center justify-center bg-amber-950 p-1">
+          <h1>My Canvas</h1>
+        </header>
+        <main className="flex flex-1 items-center justify-center overflow-auto">{children}</main>
+        <footer className="flex items-center justify-center bg-amber-950 p-1">
+          <p>
+            Created by{" "}
+            <a href="https://jakubwojtysiak.online" target="_blank" rel="noopener noreferrer">
+              jakubwojtysiak.online
+            </a>
+          </p>
+        </footer>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/(Canvas_Editor)/canvas-editor/page.tsx
@@ -0,0 +1,7 @@
+export default function CanvasEditorPage() {
+  return (
+    <div>
+      <h1>Canvas Editor</h1>
+    </div>
+  );
+}
